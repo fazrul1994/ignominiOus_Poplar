1831d8d5f9722faed56c464e0ebcbc184172d60e
Edrick Vince Sinsuan <sedrickvince@gmail.com>
2020-12-06 20:31:12 +0300

    mdss: flicker_free: Directly put calculations in set_backlight function
    - Inline backlight calculation to set_brightness function and have the calculation done after changes to bl_level are made.
    - Remove unnecessary framebuffer notifier since AOD brightness seems to depend likely more on the system not the kernel.
    
    Signed-off-by: ederekun <sedrickvince@gmail.com>

--- drivers/video/fbdev/msm/flicker_free.c
+++ drivers/video/fbdev/msm/flicker_free.c
@@ -32,6 +32,42 @@
 
 #include "mdss_mdp.h"
 
+/* Maximum value of RGB possible */
+#define FF_MAX_SCALE 32768
+
+/* Minimum value of RGB recommended */
+#define FF_MIN_SCALE 2560 
+
+/* Number of backlight entries */
+#define BACKLIGHT_INDEX 66
+
+/* Minimum backlight value that does not flicker */
+static int elvss_off_threshold = 66;
+
+/* Proc directory entries */
+static struct proc_dir_entry *root_entry, *enabled, *minbright;
+
+/* Display configuration data */
+static const int bkl_to_pcc[BACKLIGHT_INDEX] =
+	{42, 56, 67, 75, 84, 91, 98, 104, 109, 114, 119, 124, 128, 133, 136,
+	140, 143, 146, 150, 152, 156, 159, 162, 165, 168, 172, 176, 178, 181,
+	184, 187, 189, 192, 194, 196, 199, 202, 204, 206, 209, 211, 213, 215,
+	217, 220, 222, 224, 226, 228, 230, 233, 236, 237, 239, 241, 241, 243,
+	245, 246, 249, 249, 250, 252, 254, 255, 256};
+
+static const uint32_t pcc_depth[9] = {128, 256, 512, 1024, 2048,
+	4096, 8192, 16384, 32768};
+
+static struct mdp_pcc_cfg_data pcc_config;
+static struct mdp_dither_cfg_data dither_config;
+static struct mdp_dither_data_v1_7 *dither_payload;
+static struct mdp_pcc_data_v1_7 *payload;
+struct msm_fb_data_type *ff_mfd_copy;
+
+static uint32_t dither_copyback = 0;
+static uint32_t copyback = 0;
+uint32_t ff_bl_lvl_cpy;
+
 struct mdss_panel_data *pdata;
 struct mdp_pcc_cfg_data pcc_config;
 struct mdp_pcc_data_v1_7 *payload;
@@ -59,6 +95,9 @@ static void back_to_pcc(struct work_struct *work)
 	mdss_panel_calc_backlight(backlight);
 }
 #endif
+	/* Configure dither values */
+	dither_config.flags = MDP_PP_OPS_WRITE | (mdss_backlight_enable ?
+										MDP_PP_OPS_ENABLE : MDP_PP_OPS_DISABLE);
 
 static int flicker_free_push_dither(int depth)
 {
@@ -77,6 +116,10 @@ static int flicker_free_push_dither(int depth)
 
 	return mdss_mdp_dither_config(get_mfd_copy(),&dither_config,&dither_copyback,1);
 }
+
+	/* Configure pcc values */
+	pcc_config.ops = MDP_PP_OPS_WRITE | (pcc_enabled ?
+							MDP_PP_OPS_ENABLE : MDP_PP_OPS_DISABLE);
 
 static int flicker_free_push_pcc(int temp)
 {
@@ -119,10 +162,18 @@ u32 mdss_panel_calc_backlight(u32 bl_lvl)
 			pcc_enabled = false;
 			set_brightness(elvss_off_threshold);
 		}
+uint32_t mdss_panel_calc_backlight(uint32_t bl_lvl)
+{
+	if (mdss_backlight_enable && bl_lvl < elvss_off_threshold) {
+		pcc_enabled = true;
+		if (!flicker_free_push(bl_lvl))
+			return elvss_off_threshold;
+	} else if (pcc_enabled) {
+		pcc_enabled = false;
+		flicker_free_push(elvss_off_threshold);
 	}
 	return bl_lvl;
 }
-
 
 void set_flicker_free(bool enabled)
 {
@@ -157,6 +208,55 @@ void set_flicker_free(bool enabled)
 	
 } 
 
+/*
+ * Proc directory
+ */
+static ssize_t ff_write_proc(struct file *file, const char __user *buffer,
+								size_t count, loff_t *pos)
+{
+	struct mdss_panel_data *pdata;
+	int value = 0;
+	bool state;
+
+	get_user(value, buffer);
+	state = value != '0';
+
+	if (mdss_backlight_enable != state) {
+		mdss_backlight_enable = state;
+
+		if (!unlikely(ff_mfd_copy))
+			goto end;
+
+		pdata = dev_get_platdata(&ff_mfd_copy->pdev->dev);
+		if (unlikely(!pdata || !pdata->set_backlight))
+			goto end;
+
+		pdata->set_backlight(pdata, ff_bl_lvl_cpy);
+	}
+end:
+	return count;
+}
+
+static int show_ff_state(struct seq_file *seq, void *v)
+{
+	seq_printf(seq, "%d\n", mdss_backlight_enable);
+	return 0;
+}
+
+static int open_ff_proc(struct inode *inode, struct file *file)
+{
+	return single_open(file, show_ff_state, NULL);
+}
+
+static const struct file_operations proc_file_fops_state = {
+	.owner = THIS_MODULE,
+	.open = open_ff_proc,
+	.read = seq_read,
+	.write = ff_write_proc,
+	.llseek = seq_lseek,
+	.release = single_release,
+};
+
 void set_elvss_off_threshold(int value)
 {
 	elvss_off_threshold = value;
@@ -187,12 +287,33 @@ static int __init flicker_free_init(void)
 	INIT_DELAYED_WORK(&back_to_pcc_work,back_to_pcc);
 #endif
 	return 0;
+	dither_payload = kzalloc(sizeof(struct mdp_dither_data_v1_7), GFP_USER);
+
+	/* File operations init */
+	root_entry = proc_mkdir("flicker_free", NULL);
+
+	enabled = proc_create("flicker_free", 0x0666, root_entry,
+					&proc_file_fops_state);
+	if (!enabled) {
+		ret = -EINVAL;
+		goto end;
+	}
+
+	minbright = proc_create("min_brightness", 0x0666, root_entry,
+					&proc_file_fops_eot);
+	if (!minbright)
+		ret = -EINVAL;
+
+end:
+	return ret;
 }
 
 static void __exit flicker_free_exit(void)
 {
 	kfree(payload);
 	kfree(dither_payload);
+	remove_proc_entry("flicker_free", root_entry);
+	remove_proc_entry("min_brightness", root_entry);
 }
 
 late_initcall(flicker_free_init);
