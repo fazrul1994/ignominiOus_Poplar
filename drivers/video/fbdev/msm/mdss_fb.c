09351177a012661d54f89574c499c2e6c604068e
fazrul1994 <fazrulfadhilah@gmail.com>
2021-04-22 16:29:29 +0000

    Revert "mdss: flicker_free: Directly put calculations in set_backlight function"
    This reverts commit 1831d8d5f9722faed56c464e0ebcbc184172d60e.

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -2005,9 +2005,6 @@ void mdss_fb_set_backlight(struct msm_fb_data_type *mfd, u32 bkl_lvl)
 			pr_debug("backlight sent to panel :%d\n", temp);
 			pdata->set_backlight(pdata, temp);
 		#endif
-			ff_bl_lvl_cpy = temp;
-#endif
-			pdata->set_backlight(pdata, temp);
 			mfd->bl_level = bkl_lvl;
 			mfd->bl_level_scaled = temp;
 		}
@@ -2059,9 +2056,6 @@ void mdss_fb_update_backlight(struct msm_fb_data_type *mfd)
 		#else
 			pdata->set_backlight(pdata, temp);
 		#endif
-			ff_bl_lvl_cpy = temp;
-#endif
-			pdata->set_backlight(pdata, temp);
 			mfd->bl_level_scaled = mfd->unset_bl_level;
 			mfd->allow_bl_update = true;
 		}
